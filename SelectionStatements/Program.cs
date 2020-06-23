961635326fbf05c5372aa35bc67721a0636ea0d2
mraeb <mraeburn111@gmail.com>
2020-06-23 11:25:42 -0500

    intial commit

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+
+            //assign a random number inside of favNumber variable
+            var favNumber = r.Next(1, 1000);
+
+
+            Console.WriteLine($"Try to correctly guess the random number");
+
+            //convert user input(string) into an int
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too Low!!!!");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too High!!!");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+
+        }
+
+    
+    
+    
+    
+    
+    }
+    
+    
+   
+
+}
